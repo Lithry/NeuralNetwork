fa70d1e5faf49e48d6eb09002a114d4483908937
Lithry <facundosargiotti@gmail.com>
2017-12-15 16:05:16 -0300

    Chequar movimiento de Lunar y fitness

---
+++ Assets/Script/Lander.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lander : MonoBehaviour {
+	private Transform trans;
+	private NeuralNetwork brain;
+	private Vector3 plataform;
+	private float right;
+	private float left;
+	private float forward;
+	private float back;
+	private float up;
+	private float down;
+	private float speed;
+	public float fitness;
+	private Vector3 lastPosition;
+	private float distX;
+	private float lastDistX;
+	private float distZ;
+	private float lastDistZ;
+
+	void Start () {
+		trans = transform;
+	}
+
+	public void SetBrain(int inputs, int outputs, int hiddenLayers, int neuronsPerHidLayer, float bias, float sigmoidPending){
+		brain = new NeuralNetwork(inputs, outputs, hiddenLayers, neuronsPerHidLayer, bias, sigmoidPending);
+	}
+	
+	// Update is called once per frame
+	public void UpdateLander (float dt) {
+		List<float> inp = new List<float>();
+		
+		plataform = GetPlataform();
+
+		inp.Add(plataform.x);
+		inp.Add(plataform.y);
+		inp.Add(plataform.z);
+
+		List<float> outp = brain.Think(inp);
+		
+		right = outp[0];
+		left = outp[1];
+		up = outp[2];
+		down = outp[3];
+
+		//calculate steering forces
+		float movForce = ((right - left) + (up - down)) * 2;
+		Vector3 dir = new Vector3(right + left, 0, forward + back);
+
+		//update position
+		lastPosition = trans.position;
+		trans.position += (dir * dt * movForce);
+
+		distX = Mathf.Abs(trans.position.x - plataform.x);
+		distZ = Mathf.Abs(trans.position.z - plataform.z);
+
+		if (distX < lastDistX && distZ < lastDistZ){
+			IncrementFitness(10);
+		}
+
+		lastDistX = distX;
+		lastDistZ = distZ;
+	}
+
+	private Vector3 GetPlataform(){
+		GameObject plataform = MoonLanderManager.instance.GetPlataform();
+		Vector3 dir = plataform.transform.position - trans.position;
+		return dir.normalized;
+	}
+
+	public int GetNumberOfWeights(){
+		return brain.GetNumberOfWeights();
+	}
+
+	public float GetFitness(){
+		return fitness;
+	}
+
+	public void SetWeights(List<float> w){
+		brain.SetWeights(w);
+		fitness = 0;
+	}
+
+	public List<float> GetWeights(){
+		return brain.GetWeights();
+	}
+
+	public void IncrementFitness(float val){
+		fitness += val;
+	}
+}
---
+++ Assets/Script/MoonLanderManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoonLanderManager : MonoBehaviour {
+	public static MoonLanderManager instance;
+	private GeneticAlg ga;
+	private List<Lander> landers = new List<Lander>();
+	public GameObject landerPrefab;
+	public GameObject plataformPrefab;
+	private GameObject plataform;
+	public int lunarsNum;
+	public float durationOfGeneration;
+	public int inputs;
+	public int outputs;
+	public int numHiddenLayers;
+	public int numNeuronPerHiddenLayer;
+	public int elitesNum;
+
+	[Range(-1.0f, 1.0f)]
+	public float bias;
+	[Range(0.00f, 1.00f)]
+	public float mutation;
+	[Range(0.01f, 3.0f)]
+	public float sigmoidPending;
+	private int generation;
+	public Text generationText;
+	private float timer;
+	public Text timerText;
+	[Range(1, 15)]
+	public int iterations;
+	
+	void Awake () {
+		instance = this;
+		generation = 1;
+		timer = 0.0f;
+		for (int i = 0; i < lunarsNum; i++){
+			GameObject obj = Instantiate(landerPrefab, new Vector3(Random.Range(-30, 30), 0, Random.Range(-20, 20)), Quaternion.Euler(90, 0, 0));
+			Lander lan = obj.GetComponent<Lander>();
+			lan.SetBrain(inputs, outputs, numHiddenLayers, numNeuronPerHiddenLayer, bias, sigmoidPending);
+			if (i == 0)
+				ga = new GeneticAlg(elitesNum, lunarsNum, lan.GetNumberOfWeights(), mutation);
+			landers.Add(lan);
+		}
+		plataform = Instantiate(plataformPrefab, new Vector3(Random.Range(-30, 30), 0, Random.Range(-18, 0)), Quaternion.Euler(90, 0, 0));
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		generationText.text = "Generation: " + generation.ToString();
+		timerText.text = "Time: " + timer.ToString("F2");
+		for (int i = 0; i < iterations; i++){
+			for (int j = 0; j < landers.Count; j++){
+				landers[j].UpdateLander(Time.fixedDeltaTime);
+			}
+			timer += Time.fixedDeltaTime;
+		}
+		if (timer > durationOfGeneration)
+			Evolve();
+	}
+
+	public GameObject GetPlataform(){
+		return plataform;
+	}
+
+	private void Evolve(){
+		float maxFitness = landers[0].GetFitness();
+		List<Chromosome> chromList = new List<Chromosome>();
+		for (int i = 0; i < landers.Count; i++){
+			Chromosome c = new Chromosome();
+			c.fitness = landers[i].GetFitness();
+			if (maxFitness < c.fitness)
+				maxFitness = c.fitness;
+			c.weights = landers[i].GetWeights();
+			chromList.Add(c);
+		}
+		Debug.Log("Gen " + generation.ToString() + " max Fitness: " + maxFitness.ToString("F0"));
+		chromList = ga.Evolv(chromList);
+
+		for (int i = 0; i < landers.Count; i++)
+		{
+			landers[i].SetWeights(chromList[i].weights);
+		}
+
+		for (int i = 0; i < landers.Count; i++)
+		{
+			landers[i].transform.position = new Vector3(Random.Range(-30, 30), 0, Random.Range(-20, 20));
+			landers[i].transform.rotation = Quaternion.Euler(90, 0, 0);
+		}
+
+		plataform.transform.position = new Vector3(Random.Range(-30, 30), 0, Random.Range(-18, 0));
+
+		timer = 0.0f;
+		generation++;
+	}
+}
